02d93f4e09766716c17df634abec254dc8c7957c
DR-- <dr188888@163.com>
2018-08-11 15:22:08 +0800

    up

--- gzhipin-DR/src/index.js
+++ gzhipin-DR/src/index.js
@@ -3,22 +3,23 @@
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import App from './App';
+import {Provider} from 'react-redux'
 import {HashRouter, Switch, Route} from 'react-router-dom'
 
+import store from './redux/store'
 import Login from './containers/login/login'
 import Register from './containers/register/register'
 import Main from './containers/main/main'
 
 ReactDOM.render((
-  <HashRouter>
-    <Switch>
-      <Route path='/login' component={Login}/>
-      <Route path='/register' component={Register}/>
-      <Route component={Main}/>
-    </Switch>
-  </HashRouter>
-), document.getElementById('root'));
+  <Provider store={store}>
+    <HashRouter>
+      <Switch>
+        <Route path='/login' component={Login}/>
+        <Route path='/register' component={Register}/>
+        <Route component={Main}/>
+      </Switch>
+    </HashRouter>
+  </Provider>
+), document.getElementById('root'))
 
-/*
-ReactDOM.render(<App />,document.getElementById('root'));*/
--- gzhipin-DR/src/redux/store.js
+++ gzhipin-DR/src/redux/store.js
@@ -1,2 +1,12 @@
-import {createStore} from 'redux';
+/*
+redux最核心的store对象模块
+ */
+
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import reducers from './reducers'
+
+export default createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
+
 
